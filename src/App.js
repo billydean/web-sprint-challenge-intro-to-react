729f6d54c66a42a22583a3b5ace32f8ea85d80a9
billydean <billy.goehring@gmail.com>
2022-02-28 09:18:58 -0800

    'deleted' old components and began setting up new ones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
-// import Card from "./components/Card"
-import Character from "./components/Character"
+// import CharacterList component
+import CharacterList from "./components/CharacterList"
+// set up router in CharacterList
+import { BrowserRouter as Router } from 'react-router-dom';
+// import Search and Footer components
+import Search from './components/Search';
+import Footer from './components/Footer';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,55 +18,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <Character />
+      <h1 className="Header">Star Wars</h1>
+      <Search />
+      <Router>
+        <CharacterList />
+      </Router>
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-
-/*
-In this challenge you will create a web page that presents a styled list of characters obtained from an API. Being able to render out data to a web page is a large part of what JavaScript developers do, this challenge assesses your ability to achieve such a task.
-
-- [ ] Use the endpoint `[GET] https://swapi.dev/api/people` (mocked in [msw](https://github.com/mswjs/msw)) to obtain characters.
-- [ ] Set the list of characters into state.
-- [ ] Render your characters to the DOM:
-
-  1. Build a React component named 'Character' to render an individual character.
-  1. Map over the list in state, and for each character render a Character to the page.
-  1. Each rendered character must display its name in the DOM (e.g. "Luke Skywalker").
-  1. The character's name can't be hard-coded into the HTML. This data must be obtained from the API.
-  1. The components must be styled with **styled-components**.
-
-  - Data obtained from the endpoint using browser-run JavaScript is mocked with [msw](https://github.com/mswjs/msw).
-- If you test the endpoint using HTTPie or Postman you will obtain different results, as msw won't intercept the request.
-- You are welcome to create additional files but **do not move or rename existing files** or folders.
-- Do not alter your `package.json` file except to install extra libraries.
-- The `start` process can sometimes choke after adding new dependencies and may need to be restarted.
-- In your solution, it is essential that you follow best practices and produce clean and professional results.
-- Schedule time to review and polish your work, including spell-checking and grammar-checking.
-- It is better to submit a challenge that meets MVP than one that attempts too much and does not.
-
-
-API DATA:
-  - 82 count
-  - 9 pages
-  n=1
-  while n < 83
-  /api/people/${n}
-
-  name ""
-  height ""
-  mass ""
-  hair_color ""
-  skin_color ""
-  eye_color ""
-  birth_year ""
-  gender ""
-  homeworld "api"
-  films [api..]
-  species [api]
-
-*/
